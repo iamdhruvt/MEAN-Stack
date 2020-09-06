b08b12aa9062afc67ba4389bf0a99adf8b91d8f5
iamdhruvt <65751524+iamdhruvt@users.noreply.github.com>
2020-09-06 11:19:07 +0530

    Add files via upload

---
+++ MEAN Assignments/Day 1/assignment1.js
@@ -0,0 +1,19 @@
+const parent = document.querySelector("#parentBlock");
+console.log(parent);
+
+window.addEventListener("load", () => {
+    console.log("ON.LOAD");
+
+    const parent = document.querySelector("#parentBlock");
+    console.log("INSIDE.LOAD", parent);
+
+
+    for (let i = 0; i < 10; i++) {
+
+        const newElement = parent.children[0].cloneNode(true);
+        newElement.style.display = "flex";
+
+
+        parent.insertBefore(newElement, parent.firstChild);
+    }
+});
